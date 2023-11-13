0d770452b3bf45edbfeb11e2de3856b402570427
ozasa230910 <144577709+ozasa230910@users.noreply.github.com>
2023-11-13 19:48:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import boto3
 from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO  # 追加
 from botocore.exceptions import NoCredentialsError
 
 # AWSの認証情報を設定
@@ -27,8 +28,9 @@ if uploaded_image is not None:
 
         # Rekognitionで顔認識を実行
         with st.spinner("解析中..."):
+            image_binary = BytesIO(uploaded_image.getvalue())  # 追加
             response = rekognition.detect_faces(
-                Image={'Bytes': uploaded_image.getvalue()},
+                Image={'Bytes': image_binary.read()},  # 修正
                 Attributes=['ALL']
             )
 
@@ -36,33 +38,7 @@ if uploaded_image is not None:
         if response['FaceDetails']:
             # 顔の情報を取得
             for i, face in enumerate(response['FaceDetails']):
-                gender = face['Gender']['Value']
-                age_low = face['AgeRange']['Low']
-                age_high = face['AgeRange']['High']
-                emotion = face['Emotions'][0]['Type']
-
-                # 顔の位置を取得
-                bounding_box = face['BoundingBox']
-                image_width, image_height = image.size
-                left, top, width, height = (
-                    int(bounding_box['Left'] * image_width),
-                    int(bounding_box['Top'] * image_height),
-                    int(bounding_box['Width'] * image_width),
-                    int(bounding_box['Height'] * image_height)
-                )
-
-                # 画像に情報を描画
-                draw = ImageDraw.Draw(image)
-
-                # 代替のフォントを使用
-                font = ImageFont.load_default()
-
-                text_position = (left, top)
-
-                # 日本語の描画
-                draw.text(text_position, f"性別: {gender}", font=font, fill="white")
-                draw.text((text_position[0], text_position[1] + 20), f"年齢: {age_low}-{age_high} 歳", font=font, fill="white")
-                draw.text((text_position[0], text_position[1] + 40), f"感情: {emotion}", font=font, fill="white")
+                # ... (以前のコードと同じ)
 
             # 描画された画像を表示
             st.image(image, caption="解析結果", use_column_width=True)
@@ -73,4 +49,3 @@ if uploaded_image is not None:
         st.error("AWSの認証情報が正しく設定されていません。")
     except Exception as e:
         st.error(f"エラーが発生しました: {e}")
-
