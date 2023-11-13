d7c438d7f650d9a32d8bd4fa5f572048ca391308
ozasa230910 <144577709+ozasa230910@users.noreply.github.com>
2023-11-13 19:53:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import boto3
 from PIL import Image, ImageDraw, ImageFont
-from io import BytesIO  # 追加
+from io import BytesIO
 from botocore.exceptions import NoCredentialsError
 
 # AWSの認証情報を設定
@@ -28,9 +28,9 @@ if uploaded_image is not None:
 
         # Rekognitionで顔認識を実行
         with st.spinner("解析中..."):
-            image_binary = BytesIO(uploaded_image.getvalue())  # 追加
+            image_binary = BytesIO(uploaded_image.getvalue())
             response = rekognition.detect_faces(
-                Image={'Bytes': image_binary.read()},  # 修正
+                Image={'Bytes': image_binary.read()},
                 Attributes=['ALL']
             )
 
@@ -38,7 +38,30 @@ if uploaded_image is not None:
         if response['FaceDetails']:
             # 顔の情報を取得
             for i, face in enumerate(response['FaceDetails']):
-                # ... (以前のコードと同じ)
+                gender = face['Gender']['Value']
+                age_low = face['AgeRange']['Low']
+                age_high = face['AgeRange']['High']
+                emotion = face['Emotions'][0]['Type']
+
+                # 顔の位置を取得
+                bounding_box = face['BoundingBox']
+                image_width, image_height = image.size
+                left, top, width, height = (
+                    int(bounding_box['Left'] * image_width),
+                    int(bounding_box['Top'] * image_height),
+                    int(bounding_box['Width'] * image_width),
+                    int(bounding_box['Height'] * image_height)
+                )
+
+                # 画像に情報を描画
+                draw = ImageDraw.Draw(image)
+                font = ImageFont.load_default()
+                text_position = (left, top)
+
+                # 日本語の描画
+                draw.text(text_position, f"性別: {gender}", font=font, fill="white")
+                draw.text((text_position[0], text_position[1] + 20), f"年齢: {age_low}-{age_high} 歳", font=font, fill="white")
+                draw.text((text_position[0], text_position[1] + 40), f"感情: {emotion}", font=font, fill="white")
 
             # 描画された画像を表示
             st.image(image, caption="解析結果", use_column_width=True)
